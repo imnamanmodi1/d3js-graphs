83d679c9ac1fce86a6b360e8daefc8a897351e88
Naman Modi <modi.naman14@gmail.com>
2019-06-20 16:32:35 +0530

    Added Bar Chart with D3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import RaisedButton from "material-ui/RaisedButton";
+import BarChart from "./BarChart";
+import { range as d3Range } from "d3";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  state = {
+    data: d3Range(34).map(Math.random),
+    currentIndex: null
+  };
+
+  addData = () =>
+    this.setState({
+      data: [...this.state.data, Math.random()]
+    });
+
+  setCurrentIndex = currentIndex =>
+    this.setState({
+      currentIndex
+    });
+
+  render() {
+    const { data, currentIndex } = this.state;
+
+    return (
+      <MuiThemeProvider>
+        <div style={styles}>
+          <p>{"\u2728"} Click the button 👇</p>
+          <RaisedButton
+            label={"The Button"}
+            secondary={true}
+            onClick={this.addData}
+          />
+          <p>
+            {data.length} datapoints
+            <br />
+            <small>{currentIndex !== null ? data[currentIndex] : "🧐"}</small>
+          </p>
+          <svg width="100%" height="500">
+            <BarChart
+              data={data}
+              width={500}
+              height={250}
+              x={0}
+              y={0}
+              highlightBar={this.setCurrentIndex}
+              highlightedBar={currentIndex}
+            />
+          </svg>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
